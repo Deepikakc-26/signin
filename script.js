41509bce88ffa991c1499df994a39869ef81a252
Deepika K C <kcdeepika74@gmail.com>
2024-11-26 13:19:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showSignUp() {
+    document.getElementById('login-form').classList.add('hidden');
+    document.getElementById('signup-form').classList.remove('hidden');
+}
+
+function showLogin() {
+    document.getElementById('signup-form').classList.add('hidden');
+    document.getElementById('login-form').classList.remove('hidden');
+}
